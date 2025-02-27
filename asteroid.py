214802acfbff3f20c41656f01662fe18c2be1363
Trenthammer503 <c.trentham87@gmail.com>
2025-02-27 13:06:39 -0600

    asdf

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+import pygame #type:ignore
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x)), (int(self.position.y)), self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
